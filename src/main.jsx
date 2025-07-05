90e1ac85e9d3f8988ebd9971109a7efcef1a5b0d
rogercampos21 <roger.campos@inacapmail.cl>
2025-07-04 21:32:26 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext' 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import AppRouter from './routes/AppRouter'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
